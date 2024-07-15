115bfdda01775f5deec65c513c0ec66516048fb3
Noel Duggan <noelduggan21@gmail.com>
2024-07-15 14:56:51 +0000

    Addded validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -14,13 +14,36 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    if validate_data(sales_data):
+        print("Data is valid.")
+        # Proceed with further processing, e.g., updating the Google Sheet
+    else:
+        print("Invalid data. Please try again.\n")
+        get_sales_data()
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+        # Convert all string values to integers
+        values = [int(value) for value in values]
+        return True
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
 get_sales_data()
