95b2084eeb196b289f97d238b650a6caccacd693
Noel Duggan <noelduggan21@gmail.com>
2024-07-16 08:39:29 +0000

    Add sales data to sales work

--- run.py
+++ run.py
@@ -5,30 +5,33 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-]
+    ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    if validate_data(sales_data):
-        print("Data is valid.")
-        # Proceed with further processing, e.g., updating the Google Sheet
-    else:
-        print("Invalid data. Please try again.\n")
-        get_sales_data()
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -37,13 +40,28 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
-            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
-        # Convert all string values to integers
-        values = [int(value) for value in values]
-        return True
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
-get_sales_data()
+    return True
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided 
+    """
+    print("Updating sales workout...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfullly.\n")
+    
+
+
+data = get_sales_data()
+sales_data = [int(num)for num in data]
+update_sales_worksheet(sales_data)
